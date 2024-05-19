b87aaf8ecdd77f7e58aa6abd82c9d7a5eb6ce091
aliMasoodii <161247046+aliMasoodii@users.noreply.github.com>
2024-05-19 19:38:27 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Fibonachi
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a number");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(value: fib(number));
+    }
+    public static int fib(int number)
+    {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < number; i++)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+    }
+}
